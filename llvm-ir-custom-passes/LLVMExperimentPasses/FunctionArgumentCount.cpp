0b82b0d5e0dab2949af2c32ec5fb1695c60140bf
Pavel Samolysov <samolisov@gmail.com>
2018-07-31 17:54:57 +0300

    Augment the first IR analysis pass
    The pass demonstrate how to preserve the results reached by the previous
    executed passes, release working memory, do finalization and print
    information about the analysis.
    
    Signed-off-by: Pavel Samolysov <samolisov@gmail.com>

--- llvm-ir-custom-passes/LLVMExperimentPasses/FunctionArgumentCount.cpp
+++ llvm-ir-custom-passes/LLVMExperimentPasses/FunctionArgumentCount.cpp
@@ -1,5 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -9,11 +10,24 @@ namespace {
         public:
             static char ID;
             FunctionArgumentCount() : FunctionPass(ID) {}
+            virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+                AU.setPreservesAll();
+            }
             virtual bool runOnFunction(Function &F) {
                 errs() << "FunctionArgumentCount --- ";
                 errs() << F.getName() << ": ";
                 errs() << F.arg_size() << '\n';
                 return false;
+            }
+            virtual bool doFinalization(Module &M) {
+                errs() << "doFinalization(Module &M) for " << M.getName() << '\n';
+                return true;
+            }
+            virtual void print(llvm::raw_ostream &O, const Module *M) const {
+                O << "FunctionArgumentCount informs:\n\tI'm an analyser" << '\n';
+            }
+            virtual void releaseMemory() {
+                errs() << "release memory" << '\n';
             }
     };
 }
